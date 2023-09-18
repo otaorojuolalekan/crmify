2feaf5cb27eb91f6657780c2ab5ab5c40c9d5eda
Olalekan Otaoroju <otaorojuolalekan@gmail.com>
2023-09-18 22:58:09 +0100

    fixed some things locally. to push to prod server

--- api/models.py
+++ api/models.py
@@ -38,6 +38,7 @@ class Case(Base):
     subject = Column(String(100), nullable=False)
     # future should be based on preset values....
     status = Column(SqlEnum(StatusEnum), nullable=False)
+    description = Column(String(255), nullable=False)
     created_at = Column(TIMESTAMP, server_default=text('now()'))
     account_id = Column(Integer, ForeignKey(
         "accounts.id", ondelete="CASCADE"), nullable=False)
